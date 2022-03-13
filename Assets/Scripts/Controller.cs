f819e0608699584473c1d68ba85a709061a68fdd
MkCrook <malcom.kusunga@gmail.com>
2022-03-13 19:39:22 +0100

    Count v1

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,6 +8,19 @@ using UnityEngine;
 /// </summary>
 public class Controller : MonoBehaviour
 {
+    /// <summary>
+    /// Variable contenant l'animateur lié aux animations exécutées lors de l'appui sur un bouton.
+    /// </summary>
+    public Animator animator;
+
+    /// <summary>
+    /// Variable contenant la durée de l'animation s'activant lors d'un chargement vers la grille de jeu.
+    /// </summary>
+    public float launchTime = 3f;
+
+    //Interface du décompte
+    public GameObject countPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +31,34 @@ public class Controller : MonoBehaviour
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Méthodes permettant de lancer le decompte lors d'un retour vers l'interface de la grille de jeu
+    /// </summary>
+    public void LaunchCount(){
+        StartCoroutine(LoadCount());
+    }
+
+    /// <summary>
+    /// Coroutine lié au lancement de l'animation lors du décompte
+    /// </summary>
+    /// <returns>
+    /// Génere une pause de 3 secondes.
+    /// </returns>
+    IEnumerator LoadCount()
+    {
+        //Démarage de l'animation
+        countPanel.SetActive(true);
+        
+        //Génération de la pause
+        float ms = Time.deltaTime;
+        while(ms <= launchTime){
+            ms += Time. deltaTime;
+            yield return null;
+        }
+        
+        //Fermeture de l'interface du décompte
+        countPanel.SetActive(false);
     }
 }
