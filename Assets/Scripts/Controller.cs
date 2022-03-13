07411b7b5d4d0036b49c8027cabe20d50166a38e
MkCrook <malcom.kusunga@gmail.com>
2022-03-13 19:04:56 +0100

    Correction bouton

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary> 
+/// Auteur : Sterlingot Guillaume, Kusunga Malcom, Bae Jin-Young, Seghir Nassima<br>
+/// Descrption : Cette classe permet la gestion des interrations liées aux différentes actions du joueur.
+/// </summary>
 public class Controller : MonoBehaviour
 {
     // Start is called before the first frame update
